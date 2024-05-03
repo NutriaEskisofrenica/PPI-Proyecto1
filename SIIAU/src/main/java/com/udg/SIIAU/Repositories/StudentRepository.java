03f31213d7544ad820efc7db6054d077c936737d
unknown <dakotahilo17@gmail.com>
2024-05-03 07:36:15 -0600

    primero

---
+++ SIIAU/src/main/java/com/udg/SIIAU/Repositories/StudentRepository.java
@@ -0,0 +1,16 @@
+package com.udg.SIIAU.Repositories;
+
+import com.udg.SIIAU.Models.StudentModel;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+@Repository
+public interface StudentRepository extends CrudRepository<StudentModel, Long> {
+
+    public Optional<ArrayList<StudentModel>> findStudentModelByCode(String code);
+
+    public Optional<ArrayList<StudentModel>> findStudentModelByName(String name);
+}
